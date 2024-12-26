45c3b77bb494a9bf8813dfd5f6b94777d53b1f3f
zheer barzan <zheerbarzan85@outlook.com>
2024-12-26 17:34:56 +0300

    Initial Commit

---
+++ Splitzi/SplitziApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitziApp.swift
+//  Splitzi
+//
+//  Created by Zheer Barzan on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
